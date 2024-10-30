9721ba4f518661de0e16d9e413c95805af60638c
Mario Fernando <mariofernandosantacruzp@gmail.com>
2024-10-30 08:41:52 -0500

    Inicializando mycardapp

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,29 @@
-import Image from "next/image";
+// page.tsx
+import React from 'react';
 
-export default function Home() {
+const ProfileCard: React.FC = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <section className="flex items-center justify-center min-h-screen bg-gradient-to-tr from-blue-300 to-blue-600 p-4 shadow-2xl overflow-visible">
+      <main className="relative flex flex-col md:flex-row items-center bg-black text-white rounded-lg shadow-lg md:max-w-2xl p-6 overflow-visible">
+        <img
+          src="/image.jpeg"
+          alt="Img mujer"
+          className="w-32 h-32 md:w-48 md:h-48 object-cover rounded-full md:rounded-lg absolute -translate-y-12 -translate-x-0 md:-translate-x-12 md:translate-y-0"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <section className="mt-24 md:mt-0 md:ml-48 text-center md:text-left">
+          <h2 className="text-2xl font-semibold">Jess Wilson</h2>
+          <p className="text-gray-400">UX Engineer</p>
+          <p className="mt-4 text-gray-500">
+            Empowering users through captivating interfaces, turning ideas into pixel-perfect realities.
+          </p>
+          <div className="mt-6 flex space-x-3 space-y-0 md:flex-row items-center justify-center md:justify-start space-y-4 md:space-y-0 md:space-x-4">
+            <button className="px-4 py-2 border border-white rounded-full w-32">Profile</button>
+            <button className="px-4 py-2 bg-white text-black rounded-full w-32">Follow</button>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </section>
   );
-}
+};
+
+export default ProfileCard;
