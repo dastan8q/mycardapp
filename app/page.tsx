280c9e1a9ac83791d8d3f208d2063b46f4233068
Mario Fernando <mariofernandosantacruzp@gmail.com>
2024-10-30 08:59:44 -0500

    tansition responsive agregada

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const ProfileCard: React.FC = () => {
         <img
           src="/image.jpeg"
           alt="Img mujer"
-          className="w-32 h-32 md:w-48 md:h-48 object-cover rounded-full md:rounded-lg absolute -translate-y-12 -translate-x-0 md:-translate-x-12 md:translate-y-0"
+          className="w-32 h-32 md:w-48 md:h-48 object-cover rounded-full transition ease-in-out delay-50 md:rounded-lg absolute -translate-y-12 -translate-x-0 md:-translate-x-12 md:translate-y-0"
         />
         <section className="mt-24 md:mt-0 md:ml-48 text-center md:text-left">
           <h2 className="text-2xl font-semibold">Jess Wilson</h2>
@@ -16,7 +16,7 @@ const ProfileCard: React.FC = () => {
           <p className="mt-4 text-gray-500">
             Empowering users through captivating interfaces, turning ideas into pixel-perfect realities.
           </p>
-          <div className="mt-6 flex space-x-3 space-y-0 md:flex-row items-center justify-center md:justify-start space-y-4 md:space-y-0 md:space-x-4">
+          <div className="mt-6 flex space-x-3 space-y-1 md:flex-row items-center justify-center md:justify-start space-y-4 md:space-y-0 md:space-x-4">
             <button className="px-4 py-2 border border-white rounded-full w-32">Profile</button>
             <button className="px-4 py-2 bg-white text-black rounded-full w-32">Follow</button>
           </div>
